4d5f630132e8c01630dc1183816f31bcfd8b71fc
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-12-02 14:43:24 +0000

    Makes changes

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ class Block():
         # input data (known as a key) and maps it onto values of fixed sizes (hash value).
 
     def compute_hash(self):
+        """
+        A function that return the hash of the block contents.
+        """
+
         block_string = json.dumps(self.__dict__, sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
 
@@ -24,6 +28,12 @@ class Blockchain():
         self.create_genesis_block()
 
     def create_genesis_block(self):
+        """
+        A function to generate genesis block and appends it to
+        the chain. The block has index 0, previous_hash as 0, and
+        a valid hash.
+        """
+
         genesis_block = Block(0, [], time.time(), "0")
         genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
@@ -34,8 +44,10 @@ class Blockchain():
     difficulty = 2
     def proof_of_work(self, block):
         """
-        The proof-of-work system makes it progressively more difficult to perform the work required to create a new block. 
+        Function that tries different values of nonce to get a hash
+        that satisfies our difficulty criteria.
         """
+
         block.nonce = 0
         computed_hash = block.compute_hash()
         while not computed_hash.startswith('0' * Blockchain.difficulty):
@@ -45,8 +57,13 @@ class Blockchain():
     
     def add_block(self, block, proof):
         """
-        Checks to see if the previous hash matches with the last hash of the chain, and if so it will append the block to the chain.
+        A function that adds the block to the chain after verification.
+        Verification includes:
+        * Checking if the proof is valid.
+        * The previous_hash referred in the block and the hash of latest block
+          in the chain match.
         """
+
         previous_hash = self.last_block.hash
         if previous_hash != block.previous_hash:
             return False
@@ -57,6 +74,11 @@ class Blockchain():
         return True
 
     def is_valid_proof(self, block, block_hash):
+        """
+        Check if block_hash is valid hash of block and satisfies
+        the difficulty criteria.
+        """
+
         return (block_hash.startswith('0' * Blockchain.difficulty) and block_hash == block.compute_hash())
     
     def add_new_transaction(self, transaction):
@@ -64,8 +86,11 @@ class Blockchain():
 
     def mine(self):
         """
-        Once we confirm that the new block is a valid proof that satisfies the difficulty criteria we can add it to the chain.
+        This function serves as an interface to add the pending
+        transactions to the blockchain by adding them to the block
+        and figuring out Proof Of Work.
         """
+
         if not self.unconfirmed_transactions:
             return False
         
